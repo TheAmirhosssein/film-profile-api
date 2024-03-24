38e5a7a9287270488d0a5096c4d384d881eb5055
amirhossein <amirhosseinmobini9@gmail.com>
2024-03-24 11:14:32 +0000

    ✨feature: movie validation added into create movie

--- app/server/routers/movie.py
+++ app/server/routers/movie.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from server.database.database import db
 from server.models import movie_model
 from server.schemas.movie_schema import movie_serializer
@@ -11,5 +11,18 @@ router = APIRouter(prefix="/movie", tags=["movie"])
 async def add_movie(movie: movie_model.AddMovie, user=Depends(JWTBearer())):
     movie = dict(movie)
     movie["user_id"] = user["_id"]
+    movie_exist = db.movies.find(
+        {
+            "$and": [
+                {"title": movie["title"]},
+                {"director": movie["director"].lower()},
+            ]
+        }
+    )
+    if movie_exist:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Movie with same info is exists",
+        )
     db.movies.insert_one(movie)
     return movie_serializer(movie)
