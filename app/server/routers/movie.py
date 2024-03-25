d411a0af8bd0bc528964eeb91bd306d9e3cee627
amirhossein <amirhosseinmobini9@gmail.com>
2024-03-25 15:09:22 +0000

    ✨feature: watched movies by user route added

--- app/server/routers/movie.py
+++ app/server/routers/movie.py
@@ -106,8 +106,8 @@ async def unwatched_movie(object_id: str, user=Depends(JWTBearer())):
     movie = db.movies.find_one({"_id": ObjectId(object_id)})
     if movie is None:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Movie with same info is exists",
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Movie does not exist",
         )
     if ObjectId(user["_id"]) in movie.get("watched_user", []):
         db.movies.update_one(
@@ -117,3 +117,15 @@ async def unwatched_movie(object_id: str, user=Depends(JWTBearer())):
         {"_id": ObjectId(object_id)}, {"$pull": {"watched_user": user["_id"]}}
     )
     return {"detail": "movie removed from your watched list"}
+
+
+@router.get("/user-movies/{username}/", summary="user's watched list")
+async def user_movies(username: str):
+    user = db.users.find_one({"username": username})
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User does not exist",
+        )
+    movies = db.movies.find({"watched_user": ObjectId(user["_id"])})
+    return movies_serializer(movies)
